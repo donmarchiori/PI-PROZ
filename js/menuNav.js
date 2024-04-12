70d938dbb10e97ea911b66bcda4031b6ea7dad32
Davi Tavares <dvt.siqueira@gmail.com>
2024-04-12 18:15:11 -0300

    pagina de login/cadastro atualizada

--- js/menuNav.js
+++ js/menuNav.js
@@ -1,28 +1,24 @@
-console.log("nav menu");
+
 const menu = document.querySelectorAll(".menu_nav>li");
-
-
-
-
 const currentPageURL = window.location.href;
 
-console.log(currentPageURL)
+menu.forEach(estiloMenu)
 
-menu.forEach(imprimir)
+function estiloMenu(menuItem, i) {
 
-function imprimir(menuItem,i){
-
-    const a = window.location.href;
+    let a = window.location.href;
     const b = menuItem.querySelector('a');
+    a = a.replace(/#/g, '');
 
-// Criar um novo URL com o caminho relativo b e o mesmo domínio e protocolo que o URL a
-const urlB = new URL(b, a);
 
-// Verificar se os URLs são iguais
-if (saoIguais = a === urlB.href){
-    console.log(i);
-    menu[i].id = 'selecionado';;  
-}
+    // Criar um novo URL com o caminho relativo b e o mesmo domínio e protocolo que o URL a
+    const urlB = new URL(b, a);
+
+    // Verificar se os URLs são iguais
+    if (saoIguais = a === urlB.href) {
+
+        menu[i].id = 'selecionado';;
+    }
 
 }
 
